2607065a482ed2066e70f1eadf95de1a3afe745d
Alessandro Pizzorni <pizzorni.alessandro@gmail.com>
2021-11-29 08:41:37 +0100

    Added table to save scores

--- bakand/db/dbClasses.py
+++ bakand/db/dbClasses.py
@@ -7,11 +7,26 @@ db = SQLAlchemy()
 
 
 class User(UserMixin, db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(45), nullable=False)
-    guid = db.Column(db.String(16), nullable=False)
+    guid = db.Column(db.String(16), nullable=False, unique=True)
     password = db.Column(db.String(45), nullable=False)
     otp = db.Column(db.String(20))
+    scores = db.relationship('Score')
 
     def __repr__(self):
         return 'User: ' + self.username + ' GUID: ' + self.guid
+
+
+class Score(db.Model):
+    __tablename__ = 'scores'
+    id = db.Column(db.Integer, primary_key=True)
+    mid = db.Column(db.Integer, nullable=False)
+    title = db.Column(db.String(45), nullable=False)
+    artist = db.Column(db.String(100), nullable=False)
+    creator = db.Column(db.String(100), nullable=False)
+    version = db.Column(db.String(45), nullable=False)
+    score = db.Column(db.Integer, nullable=False)
+    msid = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
